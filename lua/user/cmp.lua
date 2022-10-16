f03fcf4e2f00985f10cfc846a490db0c925fbc6c
Aaron <unrealapex2@gmail.com>
2022-10-15 23:38:06 -0700

    regular borders for cmd menu

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -43,8 +43,8 @@ cmp.setup({
     end,
   },
   window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
+    -- completion = cmp.config.window.bordered(),
+    -- documentation = cmp.config.window.bordered(),
   },
   formatting = {
     fields = { "kind", "abbr", "menu" },
