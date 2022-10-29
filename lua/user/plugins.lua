0d1a4ad9b64b2a71f98db5e1d720309850090e63
Aaron <unrealapex2@gmail.com>
2022-10-28 19:29:45 -0700

    Write prose mode function in lua

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -181,7 +181,51 @@ configs.setup {
   indent = { enable = true, disable = { "yaml" } },
 }
 
--- TODO: implement prose mode function in lua
+prosed = false
+-- TODO: goyo support
+function prose()
+ if not prosed then
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = 'en_us'
+    vim.opt_local.wrap = true
+
+    -- normal mode mappings
+    vim.keymap.set('n', 'j', 'gj')
+    vim.keymap.set('n', 'k', 'gk')
+    vim.keymap.set('n', '0', 'g0')
+    vim.keymap.set('n', '$', 'g$')
+    vim.keymap.set('n', '^', 'g^')
+
+    vim.keymap.set('v', 'j', 'gj')
+    vim.keymap.set('v', 'k', 'gk')
+    vim.keymap.set('v', '0', 'g0')
+    vim.keymap.set('v', '$', 'g$')
+    vim.keymap.set('v', '^', 'g^')
+    prosed = true
+    print('Prose Mode Enabled')
+  else
+    vim.opt_local.spell = false
+    vim.opt_local.spelllang = nil
+    vim.opt_local.wrap = false
+
+    -- normal mode mappings
+    vim.keymap.set('n', 'j', 'j')
+    vim.keymap.set('n', 'k', 'k')
+    vim.keymap.set('n', '0', '0')
+    vim.keymap.set('n', '$', '$')
+    vim.keymap.set('n', '^', '^')
+
+    vim.keymap.set('v', 'j', 'j')
+    vim.keymap.set('v', 'k', 'k')
+    vim.keymap.set('v', '0', '0')
+    vim.keymap.set('v', '$', '$')
+    vim.keymap.set('v', '^', '^')
+    prosed = false
+    print('Prose Mode Disabled')
+  end
+end
+
+vim.api.nvim_create_user_command('Prose', prose, {})
 
 vim.cmd[[colorscheme tokyonight-moon]]
 
