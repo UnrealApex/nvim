1c177c59c62c0702f1f5d7e42131051524a960ab
Aaron <unrealapex2@gmail.com>
2022-10-31 22:09:12 -0700

    Use vanilla method of highlighting occurances of current word

--- lua/user/core/options.lua
+++ lua/user/core/options.lua
@@ -86,6 +86,31 @@ augroup show_whitespace
 augroup END
 ]]
 
+vim.cmd[[
+" Highlight all instances of word under cursor, when idle.
+" Useful when studying strange source code.
+" Type z/ to toggle highlighting on/off.
+nnoremap z/ :if AutoHighlightToggle()<Bar>set hls<Bar>endif<CR>
+function! AutoHighlightToggle()
+  let @/ = ''
+  if exists('#auto_highlight')
+    au! auto_highlight
+    augroup! auto_highlight
+    setl updatetime=4000
+    echo 'Highlight current word: off'
+    return 0
+  else
+    augroup auto_highlight
+      au!
+      au CursorHold * let @/ = '\V\<'.escape(expand('<cword>'), '\').'\>'
+    augroup end
+    setl updatetime=500
+    echo 'Highlight current word: ON'
+    return 1
+  endif
+endfunction
+]]
+
 
 -- matchit plugin
 vim.cmd[[packadd matchit]]
