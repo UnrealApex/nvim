44bb83ef10cde04bc9c7260fb1bcb2856479a40a
Aaron <unrealapex2@gmail.com>
2022-11-01 18:42:43 -0700

    Revert "Use vanilla method of highlighting occurances of current word"
    This reverts commit 1c177c59c62c0702f1f5d7e42131051524a960ab.

--- lua/user/plugins/coc.lua
+++ lua/user/plugins/coc.lua
@@ -59,6 +59,16 @@ end
 vim.keymap.set("n", "K", '<CMD>lua _G.show_docs()<CR>', {silent = true})
 vim.keymap.set("n", "gh", '<CMD>lua _G.show_docs()<CR>', {silent = true})
 
+
+-- Highlight the symbol and its references when holding the cursor.
+vim.api.nvim_create_augroup("CocGroup", {})
+vim.api.nvim_create_autocmd("CursorHold", {
+    group = "CocGroup",
+    command = "silent call CocActionAsync('highlight')",
+    desc = "Highlight symbol under cursor on CursorHold"
+})
+
+
 -- Symbol renaming.
 vim.keymap.set("n", "<leader>rn", "<Plug>(coc-rename)", {silent = true})
 
