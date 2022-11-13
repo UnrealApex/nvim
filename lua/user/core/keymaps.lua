409a44f5795fc315c2d52fa547e977e6f29ebeb9
Aaron <unrealapex2@gmail.com>
2022-11-13 04:00:44 -0800

    Find and replace on visual mode selection

--- lua/user/core/keymaps.lua
+++ lua/user/core/keymaps.lua
@@ -26,6 +26,56 @@ vim.keymap.set('n', '<leader><CR>', ':source $MYVIMRC<CR>')
 -- lua implementation
 -- vim.keymap.set('n', '<leader>b', ':set nomore <Bar> echo ":buffers" <Bar> :ls <Bar> :set more <CR>:b<Space>')
 
+
+-- TODO: write this in lua
+-- find and replace on current selection
+-- snippet written by Bryan Kennedy and Peter Butkovic
+-- https://stackoverflow.com/a/6171215/14111707
+vim.cmd[[
+" Escape special characters in a string for exact matching.
+" This is useful to copying strings from the file to the search tool
+" Based on this - http://peterodding.com/code/vim/profile/autoload/xolox/escape.vim
+function! EscapeString (string)
+  let string=a:string
+  " Escape regex characters
+  let string = escape(string, '^$.*\/~[]')
+  " Escape the line endings
+  let string = substitute(string, '\n', '\\n', 'g')
+  return string
+endfunction
+
+" Get the current visual block for search and replaces
+" This function passed the visual block through a string escape function
+" Based on this - https://stackoverflow.com/questions/676600/vim-replace-selected-text/677918#677918
+function! GetVisual() range
+  " Save the current register and clipboard
+  let reg_save = getreg('"')
+  let regtype_save = getregtype('"')
+  let cb_save = &clipboard
+  set clipboard&
+
+  " Put the current visual selection in the " register
+  normal! ""gvy
+  let selection = getreg('"')
+
+  " Put the saved registers and clipboards back
+  call setreg('"', reg_save, regtype_save)
+  let &clipboard = cb_save
+
+  "Escape any special characters in the selection
+  let escaped_selection = EscapeString(selection)
+
+  return escaped_selection
+endfunction
+
+" Start the find and replace command across the entire file
+vnoremap <C-r> <Esc>:%s/<c-r>=GetVisual()<cr>//g<left><left>
+]]
+
+
+-- replace occurrences of current selection
+-- vim.keymap.set('v', '<C-r>', '"hy:%s/<C-r>h//gc<left><left><left>')
+
 -- move lines up or down
 vim.keymap.set('n', '<A-j>', ':m .+1<CR>==', {silent = true})
 vim.keymap.set('n', '<A-k>', ':m .-2<CR>==', {silent = true})
