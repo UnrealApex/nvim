4947095c8ac7d82a7bf584bdaaae8f43822a0557
Aaron <79121360+UnrealApex@users.noreply.github.com>
2022-12-15 22:47:04 -0800

    treesitter: disable additional vim regex highlighting

--- lua/user/core/plugins.lua
+++ lua/user/core/plugins.lua
@@ -199,8 +199,6 @@ require('packer').startup({ function(use)
         highlight = {
           enable = true, -- false will disable the whole extension
           disable = { "" }, -- list of language that will be disabled
-          additional_vim_regex_highlighting = true,
-
         },
         indent = { enable = true, disable = { "yaml" } },
       }
