102ce6e7fae4fc8197c80c7c798478e68bd5bc64
Aaron <unrealapex2@gmail.com>
2023-01-10 22:01:13 -0800

    Load treesitter on very-lazy event

--- lua/user/core/plugins.lua
+++ lua/user/core/plugins.lua
@@ -112,7 +112,7 @@ return {
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     lazy = true,
-    event = { 'BufReadPre', 'BufNewFile' },
+    event = {'VeryLazy'},
     cmd = {
       'TSBufDisable',
       'TSBufEnable',
