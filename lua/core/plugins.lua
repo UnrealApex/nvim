ffd8bb81217e56b325570d7c184e37ec03d53c8e
Aaron <79121360+UnrealApex@users.noreply.github.com>
2023-01-17 23:21:45 +0000

    Revert "Add an additional event for plugins that load when a file is opened"
    This reverts commit e96c88127e2cccab2ebc902590c9988688fe0673.

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -31,7 +31,7 @@ return {
   {
     'echasnovski/mini.move',
     lazy = true,
-    event = { 'BufReadPre', 'BufNewFile', 'BufWinEnter' },
+    event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       require('mini.move').setup()
     end
@@ -44,7 +44,7 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     lazy = true,
-    event = { 'BufReadPre', 'BufNewFile', 'BufWinEnter' },
+    event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       require('plugins.gitsigns')
     end
@@ -90,7 +90,7 @@ return {
   {
     'lukas-reineke/indent-blankline.nvim',
     lazy = true,
-    event = { 'BufReadPre', 'BufNewFile', 'BufWinEnter' },
+    event = { 'BufReadPre', 'BufNewFile' },
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     config = function()
       require("indent_blankline").setup({
@@ -110,7 +110,7 @@ return {
   {
     'junegunn/rainbow_parentheses.vim',
     lazy = true,
-    event = { 'BufReadPre', 'BufNewFile', 'BufWinEnter' },
+    event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       vim.cmd [[RainbowParentheses]]
     end
@@ -238,7 +238,7 @@ return {
   {
     'wellle/context.vim',
     lazy = true,
-    event = { 'BufReadPre', 'BufNewFile', 'BufWinEnter' },
+    event = { 'BufReadPre', 'BufNewFile' },
     config = function()
       vim.cmd [[ContextEnable]]
     end
