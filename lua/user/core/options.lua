1765e446ff6bdd23895a346e22b98df03ffb27c2
Aaron <79121360+UnrealApex@users.noreply.github.com>
2023-02-07 12:43:37 -0800

    Update options.lua

--- lua/user/core/options.lua
+++ lua/user/core/options.lua
@@ -1,81 +1,92 @@
+-- automatic indentation
+vim.opt.autoindent = true
+-- reread file if it has been modified outside of Vim
+vim.opt.autoread = true
+-- set window background to dark
+vim.opt.background = 'dark'
+-- more powerful backspacing
+vim.opt.backspace = 'indent,eol,start'
+-- store all backup files in one directory
+vim.opt.backupdir = vim.fn.expand('~/.vim/swap//')
 -- enter the current millennium
 vim.opt.compatible = false
--- allow hidden buffers
-vim.opt.hidden = true
--- show line numbers
-vim.opt.number = true
--- show relative line numbers
-vim.opt.relativenumber = true
+vim.opt.completeopt = 'menu,menuone,noselect'
 -- enable cursor line
 vim.opt.cursorline = true
--- disable annoying error bell to prevent your ears from dying
+-- store all swap files in one directory
+vim.opt.dir = vim.fn.expand('~/.vim/swap//')
+-- disable annoying error bell
 vim.opt.errorbells = false
-vim.opt.visualbell = false
--- syntax highlighting
-vim.cmd [[syntax on]]
+vim.opt.expandtab = true
+vim.opt.fo:append('jpor')
+-- enable folding
+vim.opt.foldenable = false
+vim.opt.foldmethod = 'indent'
+-- allow hidden buffers
+vim.opt.hidden = true
+vim.opt.history = 500
+-- ignore case unless explicitly stated
+vim.opt.ignorecase = true
+-- incremental search
+vim.opt.incsearch = true
+vim.opt.list = true
+vim.opt.listchars = ''
+vim.opt.magic = true
+-- show line numbers
+vim.opt.number = true
+-- basic completion
+vim.opt.omnifunc = 'syntaxcomplete#Complete'
+vim.opt.path:append('.,**')
+vim.opt.pumheight = 15
+-- show relative line numbers
+vim.opt.relativenumber = true
+-- show cursor position in status bar
+vim.opt.ruler = true
+-- 8 lines above or below cursor when scrolling
+vim.opt.scrolloff = 8
+-- indents to next multiple of 'shiftwidth'.
+vim.opt.shiftwidth = 2
+vim.opt.shiftround = true
 vim.opt.showmode = false
 -- show typed command in status bar
 vim.opt.showcmd = true
--- show cursor position in status bar
-vim.opt.ruler = true
+vim.opt.signcolumn = 'yes'
+vim.opt.smartcase = true
+-- set tabs to two spaces
+vim.opt.tabstop = 2
+vim.opt.termguicolors = true
 -- show file in titlebar
 vim.opt.title = true
--- 8 lines above or below cursor when scrolling
-vim.opt.scrolloff = 8
--- don't wrap lines
-vim.opt.wrap = false
--- basic completion
-vim.opt.omnifunc = 'syntaxcomplete#Complete'
-vim.opt.completeopt = 'menu,menuone,noselect'
-vim.opt.list = true
-vim.opt.listchars = ''
--- automatic indentation
-vim.opt.autoindent = true
-vim.opt.fo:append('jpor')
--- more powerful backspacing
-vim.opt.backspace = 'indent,eol,start'
--- vim.opt.tabs to two spaces
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
--- indents to next multiple of 'shiftwidth'.
-vim.opt.shiftround = true
-
-vim.opt.expandtab = true
--- incremental search
-vim.opt.incsearch = true
-vim.opt.magic = true
--- ignore case unless explicitly stated
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
--- reread file if it has been modified outside of Vim
-vim.opt.autoread = true
---[[
-save yourself some memory
-you are never gonna undo 1000 steps which is the default value for undolevels
-50 is the default value for history which is relatively low to be useful.
---]]
-vim.opt.undolevels = 500
-vim.opt.history = 500
--- store all swap files in one directory
-vim.opt.dir = vim.fn.stdpath('config') .. '/swap'
--- store all backup files in one directory
-vim.opt.backupdir = vim.fn.stdpath('config') .. '/swap'
+vim.opt.undodir = vim.fn.expand('~/.vim/undo//')
 -- persistent undo tree
 vim.opt.undofile = true
-vim.opt.undodir = vim.fn.stdpath('config') .. '/undo'
-vim.opt.wildmode = 'longest:full,full'
-vim.opt.wildmenu = true
+vim.opt.undolevels = 500
 -- set updatetime to 200 milliseconds
 vim.opt.updatetime = 200
--- set window background to dark
-vim.opt.background = 'dark'
-vim.opt.termguicolors = true
--- enable folding
-vim.opt.foldmethod = 'indent'
-vim.opt.foldenable = false
+vim.opt.visualbell = false
+vim.opt.wildmenu = true
+vim.opt.wildmode = 'longest:full,full'
+vim.opt.winbar = '%=%m %f'
+-- don't wrap lines
+vim.opt.wrap = false
+
+-- syntax highlighting
+vim.cmd [[syntax on]]
+
+
+-- make sure undo and swap file directories exist
+if vim.fn.isdirectory('~/.vim/swap') == 0 and vim.fn.isdirectory('~/.vim/undo') == 0 then
+  vim.fn.mkdir(vim.fn.expand('~/.vim/swap'), 'p')
+  vim.fn.mkdir(vim.fn.expand('~/.vim/undo'), 'p')
+elseif vim.fn.isdirectory('~/.vim/swap') == 1 and vim.fn.isdirectory('~/.vim/undo') == 0 then
+  vim.fn.mkdir(vim.fn.expand('~/.vim/undo'), 'p')
+elseif vim.fn.isdirectory('~/.vim/swap') == 0 and vim.fn.isdirectory('~/.vim/undo') == 1 then
+  vim.fn.mkdir(vim.fn.expand('~/.vim/swap'), 'p')
+else
+end
+
 -- make gutter match background color
 vim.cmd [[highlight clear SignColumn]]
-vim.opt.path:append('.,**')
 
 -- show whitespaces as characters in visual mode
 vim.cmd [[
@@ -85,6 +96,16 @@ augroup show_whitespace
   autocmd Modechanged [vV\x16]*:* :set listchars-=space:·
 augroup END
 ]]
+
+
+--globals
+
+-- enable filetype.lua
+vim.g.do_filetype_lua = 1
+vim.g.did_load_filetypes = 0
+
+-- set the mapleader to space
+vim.g.mapleader = ' '
 
 -- declare before indent-blankline is loaded
 vim.g.indent_blankline_filetype_exclude = {
@@ -102,19 +123,8 @@ vim.g.indent_blankline_filetype_exclude = {
   'sh'
 }
 
-vim.g['rainbow#pairs'] = {{'(', ')'}, {'[', ']'}, {'{', '}'}}
+vim.g['rainbow#pairs'] = { { '(', ')' }, { '[', ']' }, { '{', '}' } }
 
--- load the matchit plugin
-vim.cmd [[packadd matchit]]
-
--- enable filetype.lua
-vim.g.do_filetype_lua = 1
-vim.g.did_load_filetypes = 0
-
--- set the mapleader to space
-vim.g.mapleader = ' '
-
-vim.g.netrw_keepdir = 0
 
 function LargeFileHandler()
   vim.notify(
@@ -140,74 +150,55 @@ function LargeFileChecker()
   end
 end
 
-
 -- TODO: find a way to make these two autocommands one
 vim.cmd [[
-augroup LargeFileDetection
+  augroup LargeFileDetection
     autocmd!
     autocmd BufReadPre * lua LargeFileChecker()
     autocmd FileReadPre * lua LargeFileChecker()
-augroup END
-]]
+  augroup END
+  ]]
 
 
 prosed = false
 function prose()
   -- make sure zen-mode.nvim is installed
-  if packer_plugins["zen-mode.nvim"] then
-    -- toggle prose mode
-    if not prosed then
-      -- enable spellcheck and line wrapping
-      vim.opt_local.spell = true
-      vim.opt_local.spelllang = 'en_us'
-      vim.opt_local.wrap = true
+  -- toggle prose mode
+  if not prosed then
+    -- enable spellcheck and line wrapping
+    vim.opt_local.spell = true
+    vim.opt_local.spelllang = 'en_us'
+    vim.opt_local.wrap = true
 
-      -- normal mode mappings
-      vim.keymap.set('n', 'j', 'gj')
-      vim.keymap.set('n', 'k', 'gk')
-      vim.keymap.set('n', '0', 'g0')
-      vim.keymap.set('n', '$', 'g$')
-      vim.keymap.set('n', '^', 'g^')
+    -- normal and visual mode mappings
+    vim.keymap.set({'n', 'v'}, 'j', 'gj')
+    vim.keymap.set({'n', 'v'}, 'k', 'gk')
+    vim.keymap.set({'n', 'v'}, '0', 'g0')
+    vim.keymap.set({'n', 'v'}, '$', 'g$')
+    vim.keymap.set({'n', 'v'}, '^', 'g^')
 
-      -- visual mode mappings
-      vim.keymap.set('v', 'j', 'gj')
-      vim.keymap.set('v', 'k', 'gk')
-      vim.keymap.set('v', '0', 'g0')
-      vim.keymap.set('v', '$', 'g$')
-      vim.keymap.set('v', '^', 'g^')
-      prosed = true
+    prosed = true
+    if vim.fn.exists(':ZenMode') == 2 then
       vim.cmd [[ZenMode]]
-      vim.notify('Prose Mode Enabled')
-    else
-      -- disable spellcheck and line wrapping
-      vim.opt_local.spell = false
-      vim.opt_local.spelllang = nil
-      vim.opt_local.wrap = false
-
-      -- reset normal mode mappings
-      vim.keymap.set('n', 'j', 'j')
-      vim.keymap.set('n', 'k', 'k')
-      vim.keymap.set('n', '0', '0')
-      vim.keymap.set('n', '$', '$')
-      vim.keymap.set('n', '^', '^')
-
-      -- reset visual mode mappings
-      vim.keymap.set('v', 'j', 'j')
-      vim.keymap.set('v', 'k', 'k')
-      vim.keymap.set('v', '0', '0')
-      vim.keymap.set('v', '$', '$')
-      vim.keymap.set('v', '^', '^')
-      prosed = false
-      vim.cmd [[ZenMode]]
-      vim.notify('Prose Mode Disabled')
     end
+    vim.notify('Prose Mode Enabled')
   else
-    vim.notify(
-      "error: zen-mode.nvim is not installed!" ..
-      "\n" ..
-      "prose mode will not function without it!",
-      vim.log.levels.ERROR
-    )
+    -- disable spellcheck and line wrapping
+    vim.opt_local.spell = false
+    vim.opt_local.spelllang = nil
+    vim.opt_local.wrap = false
+
+    -- reset normal and visual mode mappings
+    vim.keymap.del({'n', 'v'}, 'j')
+    vim.keymap.del({'n', 'v'}, 'k')
+    vim.keymap.del({'n', 'v'}, '0')
+    vim.keymap.del({'n', 'v'}, '$')
+    vim.keymap.del({'n', 'v'}, '^')
+    prosed = false
+    if vim.fn.exists(':ZenMode') == 2 then
+      vim.cmd [[ZenMode]]
+    end
+    vim.notify('Prose Mode Disabled')
   end
 end
 
@@ -219,11 +210,31 @@ vim.api.nvim_create_user_command('Prose', prose, {})
 -- $CONFIG
 vim.env.CONFIG = vim.fn.stdpath('config')
 
--- $OPTIONS
-vim.env.OPTIONS = vim.fn.stdpath('config') .. "\\lua\\user\\core\\options.lua"
+-- handle os file path variations
+if vim.fn.has('unix') == 1 then
+  -- $OPTIONS
+  vim.env.OPTIONS = vim.fn.stdpath('config') .. "/lua/user/core/options.lua"
 
--- $PLUGINS
-vim.env.PLUGINS = vim.fn.stdpath('config') .. "\\lua\\user\\core\\plugins.lua"
+  -- $LAZY
+  vim.env.LAZY = vim.fn.stdpath('config') .. "/lua/user//core/lazy.lua"
 
--- $KEYMAPS
-vim.env.KEYMAPS = vim.fn.stdpath('config') .. "\\lua\\user\\core\\keymaps.lua"
+  -- $PLUGINS
+  vim.env.PLUGINS = vim.fn.stdpath('config') .. "/lua/user/core/plugins.lua"
+
+  -- $KEYMAPS
+  vim.env.KEYMAPS = vim.fn.stdpath('config') .. "/lua/user/core/keymaps.lua"
+elseif vim.fn.has('win32') == 1 then
+
+  -- $OPTIONS
+  vim.env.OPTIONS = vim.fn.stdpath('config') .. "\\lua\\user\\core\\options.lua"
+
+  -- $LAZY
+  vim.env.LAZY = vim.fn.stdpath('config') .. "\\lua\\user\\core\\lazy.lua"
+
+  -- $PLUGINS
+  vim.env.PLUGINS = vim.fn.stdpath('config') .. "\\lua\\user\\core\\plugins.lua"
+
+  -- $KEYMAPS
+  vim.env.KEYMAPS = vim.fn.stdpath('config') .. "\\lua\\user\\core\\keymaps.lua"
+else
+end
